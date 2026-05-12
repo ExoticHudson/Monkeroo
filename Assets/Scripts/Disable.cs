5d00dceb0cddf0301d18942fd0fc0944c32587f9
ExoticHudson <hudsonkraushaar1@gmail.com>
2026-05-11 18:09:52 -0600

    radio

--- Assets/Scripts/Disable.cs
+++ Assets/Scripts/Disable.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enable : MonoBehaviour
+public class Disable : MonoBehaviour
 {
     public GameObject thing;
 
-        void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
         if (other.tag == "HandTag")
         {
-            thing.SetActive(true);
+            thing.SetActive(false);
         }
     }
 }
